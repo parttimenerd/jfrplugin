8802b3ffd06f82f654839ebc372df39d086b4822
GitHub Action <action@github.com>
2022-07-17 12:24:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/parttimenerd/jfrplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/parttimenerd/jfrplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.parttimenerd.jfrplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.parttimenerd.jfrplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/parttimenerd/jfrplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/parttimenerd/jfrplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.parttimenerd.jfrplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.parttimenerd.jfrplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
